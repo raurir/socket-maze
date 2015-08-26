4df28543527fb0cd354234647b5a98a16a2f4398
raurir <raurir@gmail.com>
2015-08-26 10:07:33 +1000

    restored port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var maze = require('./maze');
 var constants = require('../constants');
 
-var port = 8050;
+var port = 8010;
 var connections = {};
 var con = console;
 
